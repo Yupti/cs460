9c576ec08d992034fe691a94289fb1a5e549cbfc
Aaron Reyes <anreyes@cpp.edu>
2018-01-18 18:24:35 -0800

    problem with vigenere, when modding

--- Assignment #1 P1.py
+++ Assignment #1 P1.py
@@ -34,7 +34,15 @@ def menu():
 		playfairCipher(compressString, keyword, choice2)
 
 	elif choice == 3:
-		print("Calls Vignere ditto")
+		text = input("Enter a word or sentence you wish to encrypt/decrypt: ")
+		text = text.lower()
+		keyword = input("Enter a keyword to use for encryption/decryption: ")
+		choice2 = int(input("Enter the number for desired action: (1) Encrypt (2) Decrypt: ")) 
+
+		compressString = wordCompress(text)
+
+		vigenereEncryptDecrypt(text, keyword, choice2
+			)
 	elif choice == 4:
 		return False
 	else:
@@ -85,136 +93,142 @@ def caesarEncryptDecrypt(text, selection): #if greater than 120, minus 23
 		print("Invalid choice, returning to main menu.")
 
 def playfairCipher(text, keyword, selection): #IN PROGRESS, NEED TO RESOLVE I/J INCIDENT
-	matrixContent = []
-	matrix = [] # creates empty list to fill with the letters
-	extra = '' # holds letter if the pair is a duplicate, uses for next pair if available
-	finalText = '' # for holding plain/ciphertext when finished
-	letters = list(string.ascii_lowercase)
-	size = len(text)
+	if selection == 1 or selection == 2:
+		matrixContent = []
+		matrix = [] # creates empty list to fill with the letters
+		extra = '' # holds letter if the pair is a duplicate, uses for next pair if available
+		finalText = '' # for holding plain/ciphertext when finished
+		letters = list(string.ascii_lowercase)
+		size = len(text)
 
-	letters.remove('j') # removing to simplify matrix, NOT SURE IF LEGAL
-	# print("Letters: ", letters)
+		letters.remove('j') # removing to simplify matrix, NOT SURE IF LEGAL
+		# print("Letters: ", letters)
 
-	for i in keyword: # creates letters from keyword without duplicates
-		if i not in matrixContent:
-			matrixContent.append(i)
+		for i in keyword: # creates letters from keyword without duplicates
+			if i not in matrixContent:
+				matrixContent.append(i)
 
-	for i in range(25): # creates all needed letters for matrix
-		singleLetter = letters[i]
-		if singleLetter not in matrixContent:
-			matrixContent.append(singleLetter)
-
-	for i in range(5):
-		row = []
-		for j in range(5):
-			row.append(matrixContent.pop(0)) 
-		matrix.append(row)
-
-	textLetters = [i for i in text] # holds all letters of plain/ciphertext
-	
-	# print("Matrix:") # only for show, will remove later
-	# for i in matrix:
-	# 	print(i)
-
-	while textLetters: # separate text 2 by 2, placing duplicate pairs in 'extras'
-		letter1 = ''
-		letter2 = ''
-		location1 = [] # holds (x,y) for letter1
-		location2 = [] # holds (x,y) for letter2
-		pointer1 = 0 # pointers to use for locating the encrypted/decrypted letter to use
-		pointer2 = 0
-
-		if extra: # checks 'extra' contains a letter
-			letter1 = extra
-			extra = '' # resets to empty
-		else:
-			letter1 = textLetters.pop(0)
-
-		if textLetters: # checks if contains letters
-			letter2 = textLetters.pop(0)
-		else:
-			letter2 = 'x'
-
-		if letter1 == letter2: # checks if pair is duplicate or not
-			extra = letter2
-			letter2 = 'x' # the default letter if pair is duplicates
-
-		# print("Letter1:", letter1)
-		# print("Letter2:", letter2)
+		for i in range(25): # creates all needed letters for matrix
+			singleLetter = letters[i]
+			if singleLetter not in matrixContent:
+				matrixContent.append(singleLetter)
 
 		for i in range(5):
+			row = []
 			for j in range(5):
-				if matrix[i][j] == letter1:
-					location1.extend([i,j]) # holds coordinates
-				if matrix[i][j] == letter2:
-					location2.extend([i,j]) # same as above comment
+				row.append(matrixContent.pop(0)) 
+			matrix.append(row)
 
-		# print("Coordinates1:", location1[0], " ", location1[1])
-		# print("Coordinates2:", location2[0], " ", location2[1])
+		textLetters = [i for i in text] # holds all letters of plain/ciphertext
+	
+		# print("Matrix:") # only for show, will remove later
+		# for i in matrix:
+		# 	print(i)
 
-		if selection == 1: # FOR ENCRYPTION
-			if location1[0] == location2[0]: # case where they are in same row
-				pointer1 = location1[1] + 1
-				pointer2 = location2[1] + 1 # OK FOR NOW, NEEDS TESTING
+		while textLetters: # separate text 2 by 2, placing duplicate pairs in 'extras'
+			letter1 = ''
+			letter2 = ''
+			location1 = [] # holds (x,y) for letter1
+			location2 = [] # holds (x,y) for letter2
+			pointer1 = 0 # pointers to use for locating the encrypted/decrypted letter to use
+			pointer2 = 0
 
-				if pointer1 > 4: # to the right is out of bounds, resets to 0
-					pointer1 = 0
+			if extra: # checks 'extra' contains a letter
+				letter1 = extra
+				extra = '' # resets to empty
+			else:
+				letter1 = textLetters.pop(0)
 
-				if pointer2 > 4: # same comments as above
-					pointer2 = 0
+			if textLetters: # checks if contains letters
+				letter2 = textLetters.pop(0)
+			else:
+				letter2 = 'x'
 
-				finalText += matrix[location1[0]][pointer1] 
-				finalText += matrix[location2[0]][pointer2]
+			if letter1 == letter2: # checks if pair is duplicate or not
+				extra = letter2
+				letter2 = 'x' # the default letter if pair is duplicates
 
-			elif location1[1] == location2[1]: # case where they are in same column
-				pointer1 = location1[0] + 1
-				pointer2 = location2[0] + 1 # encrypt +1, decrypt -1
+			# print("Letter1:", letter1)
+			# print("Letter2:", letter2)
 
-				if pointer1 > 4: # to the right is out of bounds, resets to 0
-					pointer1 = 0
+			for i in range(5):
+				for j in range(5):
+					if matrix[i][j] == letter1:
+						location1.extend([i,j]) # holds coordinates
+					if matrix[i][j] == letter2:
+						location2.extend([i,j]) # same as above comment
 
-				if pointer2 > 4: # same comments as above
-					pointer2 = 0
+			# print("Coordinates1:", location1[0], " ", location1[1])
+			# print("Coordinates2:", location2[0], " ", location2[1])
 
-				finalText += matrix[pointer1][location1[1]]
-				finalText += matrix[pointer2][location2[1]]
+			if selection == 1: # FOR ENCRYPTION
+				if location1[0] == location2[0]: # case where they are in same row
+					pointer1 = location1[1] + 1
+					pointer2 = location2[1] + 1 # OK FOR NOW, NEEDS TESTING
 
-			else: # case of different rows, different columns
-				finalText += matrix[location1[0]][location2[1]]
-				finalText += matrix[location2[0]][location1[1]]
+					if pointer1 > 4: # to the right is out of bounds, resets to 0
+						pointer1 = 0
 
-		elif selection == 2: # FOR DECRYPTION
-			if location1[0] == location2[0]: # case where they are in same row
-				pointer1 = location1[1] - 1
-				pointer2 = location2[1] - 1 # OK FOR NOW, NEEDS TESTING
+					if pointer2 > 4: # same comments as above
+						pointer2 = 0
 
-				if pointer1 < 0: # to the right is out of bounds, resets to 0
-					pointer1 = 4
+					finalText += matrix[location1[0]][pointer1] 
+					finalText += matrix[location2[0]][pointer2]
 
-				if pointer2 < 0: # same comments as above
-					pointer2 = 4
+				elif location1[1] == location2[1]: # case where they are in same column
+					pointer1 = location1[0] + 1
+					pointer2 = location2[0] + 1 # encrypt +1, decrypt -1
 
-				finalText += matrix[location1[0]][pointer1] 
-				finalText += matrix[location2[0]][pointer2]
+					if pointer1 > 4: # to the right is out of bounds, resets to 0
+						pointer1 = 0
 
-			elif location1[1] == location2[1]: # case where they are in same column
-				pointer1 = location1[0] - 1
-				pointer2 = location2[0] - 1 # encrypt +1, decrypt -1
+					if pointer2 > 4: # same comments as above
+						pointer2 = 0
 
-				if pointer1 < 0: # to the right is out of bounds, resets to 0
-					pointer1 = 4
+					finalText += matrix[pointer1][location1[1]]
+					finalText += matrix[pointer2][location2[1]]
 
-				if pointer2 < 0: # same comments as above
-					pointer2 = 4
+				else: # case of different rows, different columns
+					finalText += matrix[location1[0]][location2[1]]
+					finalText += matrix[location2[0]][location1[1]]
 
-				finalText += matrix[pointer1][location1[1]]
-				finalText += matrix[pointer2][location2[1]]
+			elif selection == 2: # FOR DECRYPTION
+				if location1[0] == location2[0]: # case where they are in same row
+					pointer1 = location1[1] - 1
+					pointer2 = location2[1] - 1 # OK FOR NOW, NEEDS TESTING
 
-			else: # case of different rows, different columns
-				finalText += matrix[location1[0]][location2[1]]
-				finalText += matrix[location2[0]][location1[1]]
+					if pointer1 < 0: # to the right is out of bounds, resets to 0
+						pointer1 = 4
 
-	print("Cyphertext:", text, "\nKeyword:", keyword, "\nPlaintext:", finalText)
+					if pointer2 < 0: # same comments as above
+						pointer2 = 4
+
+					finalText += matrix[location1[0]][pointer1] 
+					finalText += matrix[location2[0]][pointer2]
+
+				elif location1[1] == location2[1]: # case where they are in same column
+					pointer1 = location1[0] - 1
+					pointer2 = location2[0] - 1 # encrypt +1, decrypt -1
+
+					if pointer1 < 0: # to the right is out of bounds, resets to 0
+						pointer1 = 4
+
+					if pointer2 < 0: # same comments as above
+						pointer2 = 4
+
+					finalText += matrix[pointer1][location1[1]]
+					finalText += matrix[pointer2][location2[1]]
+
+				else: # case of different rows, different columns
+					finalText += matrix[location1[0]][location2[1]]
+					finalText += matrix[location2[0]][location1[1]]
+
+		if selection == 1:
+			print("Plaintext:", text, "\nKeyword:", keyword, "\nCiphertext:", finalText)
+		else:	
+			print("Cyphertext:", text, "\nKeyword:", keyword, "\nPlaintext:", finalText)
+	else:
+		print("Invalid choice, returning to main menu.")
 
 
 def vigenereEncryptDecrypt(text, keyword, selection): # NOTE: keyword should be shorter than text
@@ -259,6 +273,9 @@ def vigenereEncryptDecrypt(text, keyword, selection): # NOTE: keyword should be 
 			for j in vigenere:
 				if i == vigenere[j]:
 					finalText += j
+
+		print("Plaintext:", text, "\nKeyword:", keyword, "\nCiphertext:", finalText)
+
 	elif selection == 2: # for decryption
 		for i in range(len(text)): # for iterating through entire list of text
 			if keywordCounter == keyLength:
@@ -274,6 +291,8 @@ def vigenereEncryptDecrypt(text, keyword, selection): # NOTE: keyword should be 
 			for j in vigenere:
 				if i == vigenere[j]:
 					finalText += j
+
+		print("Plaintext:", text, "\nKeyword:", keyword, "\nCiphertext:", finalText)
 	else:
 		print("Invalid choice, returning to main menu.")
  
